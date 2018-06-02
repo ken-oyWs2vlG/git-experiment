503971e0f333d44c4c2b3d68df1f385c118db6cc
Ken Sedgwick <kensedgwick@hotmail.com>
2018-06-01 19:56:10 -0700

    added experiment.js

---
+++ experiment.js
@@ -0,0 +1,3 @@
+const say = console.log;
+
+say('For he to-day that sheds his blood with me\nShall be my brother; be he ne\'er so vile,\nThis day shall gentle his condition;\nAnd gentlemen in England now a-bed\nShall think themselves accurs\'d they were not here,\nAnd hold their manhoods cheap whiles any speaks\nThat fought with us upon Saint Crispin\'s day.');
