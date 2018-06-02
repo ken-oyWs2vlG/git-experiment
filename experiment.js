b10af97e158172096a7c75002fcda244fe6b3c00
Kenneth Sedgwick <kensedgwick@hotmail.com>
2018-06-01 21:40:09 -0700

    added space

--- experiment.js
+++ experiment.js
@@ -1,6 +1,7 @@
 const say = console.log;
 
 say();
+say();
 say('\u001b[40m\u001b[31;1m' + 'For he to-day that sheds his blood with me' + '\u001b[0m');
 say('\u001b[40m\u001b[31;1m' + 'Shall be my brother; be he ne\'er so vile,' + '\u001b[0m');
 say('\u001b[40m\u001b[31;1m' + 'This day shall gentle his condition;' + '\u001b[0m');
@@ -9,7 +10,7 @@ say('\u001b[40m\u001b[31;1m' + 'Shall think themselves accurs\'d they were not h
 say('\u001b[40m\u001b[31;1m' + 'And hold their manhoods cheap whiles any speaks' + '\u001b[0m');
 say('\u001b[40m\u001b[31;1m' + 'That fought with us upon Saint Crispin\'s day.' + '\u001b[0m');
 say();
-
+say();
 
 
 /////////////////
