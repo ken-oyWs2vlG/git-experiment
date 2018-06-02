c95cefb87c30ff7d5c60c7cc070e416a107cac1b
Kenneth Sedgwick <kensedgwick@hotmail.com>
2018-06-01 20:03:54 -0700

    split text into multiple lines

--- experiment.js
+++ experiment.js
@@ -1,3 +1,9 @@
 const say = console.log;
 
-say('For he to-day that sheds his blood with me\nShall be my brother; be he ne\'er so vile,\nThis day shall gentle his condition;\nAnd gentlemen in England now a-bed\nShall think themselves accurs\'d they were not here,\nAnd hold their manhoods cheap whiles any speaks\nThat fought with us upon Saint Crispin\'s day.');
+say('For he to-day that sheds his blood with me');
+say('Shall be my brother; be he ne\'er so vile,');
+say('This day shall gentle his condition;');
+say('And gentlemen in England now a-bed');
+say('Shall think themselves accurs\'d they were not here,');
+say('And hold their manhoods cheap whiles any speaks');
+say('That fought with us upon Saint Crispin\'s day.');
