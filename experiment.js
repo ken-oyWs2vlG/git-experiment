a5d944faa06af69c5d891dc76cdd5b97193f53db
Kenneth Sedgwick <kensedgwick@hotmail.com>
2018-06-01 20:21:13 -0700

    added color

--- experiment.js
+++ experiment.js
@@ -1,9 +1,11 @@
 const say = console.log;
 
-say('For he to-day that sheds his blood with me');
-say('Shall be my brother; be he ne\'er so vile,');
-say('This day shall gentle his condition;');
-say('And gentlemen in England now a-bed');
-say('Shall think themselves accurs\'d they were not here,');
-say('And hold their manhoods cheap whiles any speaks');
-say('That fought with us upon Saint Crispin\'s day.');
+say();
+say('\u001b[40m\u001b[31;1m' + 'For he to-day that sheds his blood with me' + '\u001b[0m');
+say('\u001b[40m\u001b[31;1m' + 'Shall be my brother; be he ne\'er so vile,' + '\u001b[0m');
+say('\u001b[40m\u001b[31;1m' + 'This day shall gentle his condition;' + '\u001b[0m');
+say('\u001b[40m\u001b[31;1m' + 'And gentlemen in England now a-bed' + '\u001b[0m');
+say('\u001b[40m\u001b[31;1m' + 'Shall think themselves accurs\'d they were not here,' + '\u001b[0m');
+say('\u001b[40m\u001b[31;1m' + 'And hold their manhoods cheap whiles any speaks' + '\u001b[0m');
+say('\u001b[40m\u001b[31;1m' + 'That fought with us upon Saint Crispin\'s day.' + '\u001b[0m');
+say();
